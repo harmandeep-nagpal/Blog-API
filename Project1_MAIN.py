8aefa72d4e92aa94e401c2864c34474594382635
Harmandeep Nagpal <harmandeep01633@gmail.com>
2026-06-28 23:13:48 +0530

    Implement CRUD operations for Blog API

--- Project1_MAIN.py
+++ Project1_MAIN.py
@@ -1,17 +1,87 @@
 # We need to install pip install fastapi uvicorn sqlachemy psycopg2-binary python-dotenv
 
-from fastapi import FastAPI 
-from Project1_DATABASE import engine 
-import Project1_MODEL as models
+from fastapi import FastAPI , Depends , HTTPException
+from sqlalchemy.orm import Session
+from Project1_DATABASE import engine , SessionLocal
+import Project1_MODEL as models , Project1_Schemas as schemas
 
 models.Base.metadata.create_all(bind=engine) 
 # Explanation in 24
 
 app = FastAPI()
 
-@app.get("/")
+# DB Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+# Home Route
+@app.get("/") 
 def home():
     return{
         "message" : "Blog API Started"
     }
 
+# Create Blog
+@app.post("/blogs" , response_model=schemas.BlogResponse)
+def create_blog(blog : schemas.BlogCreate , db : Session = Depends(get_db)):
+    new_blog = models.Blog(
+        title=blog.title,
+        content=blog.content)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+# Read all blog 
+@app.get("/blogs" , response_model = list[schemas.BlogResponse])
+def get_blogs(db:Session = Depends(get_db)):
+    return db.query(models.Blog).all()
+
+#Read one blog
+@app.get("/blogs/{id}", response_model=schemas.BlogResponse)
+def get_blog(id:int,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    return blog
+
+#Update Blog API
+@app.put("/blogs/{id}",response_model=schemas.BlogResponse)
+def update_blog(
+    id:int,
+    blog:schemas.BlogCreate,
+    db: Session = Depends(get_db)
+    ):
+    existing_blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not existing_blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    
+    existing_blog.title = blog.title
+    existing_blog.content = blog.content
+
+    db.commit()
+
+    return existing_blog
+
+#Delete Blog API
+@app.delete("/blogs/{id}")
+def delete_blog(id:int,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=404, detail="Blog not found")
+    
+    blog.delete()
+    db.commit()
+
+    return{
+        "message":"Blog deleted Successfully"
+    }
